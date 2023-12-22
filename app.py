3865045449831a7c9ce0aed858296dfd6691d5bc
Jonah Schlie <82595001+jonahschlie@users.noreply.github.com>
2023-12-22 22:42:00 +0100

    added sign up screen

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def login():
     return render_template('login.html')
 
 
 @app.route("/home", methods=["POST"])
-def go_home():
+def home():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    form_name = request.form.get('form_name')
+
+    print(f"Received form from {form_name}: {email}, {password}")
+
     return render_template('home.html')
 
+@app.route("/register")
+def register():
+    return render_template('register.html')
 
 app.run(debug=True)

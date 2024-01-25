c5d0c6b289b0d8368d0e0f4c7c697af61d91d410
jonahschlie <jonah.schlie@gmail.com>
2024-01-25 12:29:44 +0100

    added mail for password reset

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ app.config.update(
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT=465,
     MAIL_USE_SSL=True,
-    MAIL_USERNAME='',
+    MAIL_USERNAME='ai.and.the.web@gmail.com',
     MAIL_PASSWORD=''
 )
 
